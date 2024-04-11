24febdccad36ac0ee7ef9bae112b687f6dede1a1
Akito Shitara <danpany@AkitonoMacBook-Pro.local>
2024-04-12 04:24:06 +0900

    add: add_typer

--- ta-cli/main.py
+++ ta-cli/main.py
@@ -1,7 +1,11 @@
 import typer
 from rich.console import Console
 
+from ta_cli.typers import db_migration
+
 app = typer.Typer()
+
+app.add_typer(db_migration.app, name="db-migration")
 
 error_console = Console(stderr=True)
 
