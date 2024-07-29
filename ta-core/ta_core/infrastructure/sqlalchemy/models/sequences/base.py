98dc8b377bfd132c49791650e343b1ec99d9ce00
Akito Shitara <himura@Akitos-MacBook-Pro.local>
2024-07-29 10:01:47 +0900

    feat: sequence table

--- ta-core/ta_core/infrastructure/sqlalchemy/models/sequences/base.py
+++ ta-core/ta_core/infrastructure/sqlalchemy/models/sequences/base.py
@@ -1,11 +1,13 @@
 from typing import Any
 
-from sqlalchemy.dialects.mysql import VARCHAR
+from sqlalchemy import select
+from sqlalchemy.dialects.mysql import BIGINT
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import Mapped, mapped_column
 
 from ta_core.infrastructure.db.settings import DB_SEQUENCE_CONNECTION_KEY
 from ta_core.infrastructure.sqlalchemy.models.base import AbstractBase
+from ta_core.use_case.unit_of_work_base import IUnitOfWork
 
 
 class AbstractSequenceBase(AbstractBase):
@@ -22,4 +24,18 @@ class AbstractSequenceBase(AbstractBase):
 class AbstractSequenceId(AbstractSequenceBase):
     __abstract__ = True
 
-    id: Mapped[str] = mapped_column(VARCHAR(36), primary_key=True, autoincrement=False)
+    id: Mapped[int] = mapped_column(
+        BIGINT(unsigned=True), primary_key=True, autoincrement=False
+    )
+
+    @classmethod
+    async def id_generator(cls, uow: IUnitOfWork) -> int:
+        record = (await uow.execute_async(select(cls))).scalar_one_or_none()
+        if record is None:
+            new_id = 0
+        else:
+            new_id = record.id + 1
+            await uow.delete_async(record)
+        model = cls(id=new_id)
+        uow.add(model)
+        return new_id
