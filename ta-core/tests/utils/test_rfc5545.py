80424685fcf827deca301f05ad59c54e56ca85f2
himura-shitara <mitarashidango0927@gmail.com>
2024-12-03 18:49:32 +0900

    refactor: make parse_recurrence's return value nullable

--- ta-core/tests/utils/test_rfc5545.py
+++ ta-core/tests/utils/test_rfc5545.py
@@ -2,8 +2,8 @@ from datetime import UTC, date, datetime
 
 import pytest
 
-from ta_core.features.event import Frequency, RecurrenceRule, Weekday
-from ta_core.utils.rfc5545_parser import parse_recurrence, parse_rrule
+from ta_core.features.event import Frequency, Recurrence, RecurrenceRule, Weekday
+from ta_core.utils.rfc5545 import parse_recurrence, parse_rrule
 
 
 @pytest.mark.parametrize(
@@ -143,8 +143,13 @@ def test_parse_rrule_error(
 
 
 @pytest.mark.parametrize(
-    "recurrence_list, is_all_day, expected_rrule, expected_rdate, expected_exdate",
+    "recurrence_list, is_all_day, expected_recurrence",
     [
+        (
+            [],
+            True,
+            None,
+        ),
         (
             [
                 "RRULE:FREQ=DAILY;UNTIL=20000101;INTERVAL=1;BYSECOND=0;BYMINUTE=0;BYHOUR=0;BYDAY=-2MO,-1TU,WE,1TH,2FR;BYMONTHDAY=1,2,3,4,5;BYYEARDAY=1,2,3,4,5;BYWEEKNO=1,2,3,4,5;BYMONTH=1,2,3,4,5;BYSETPOS=1,2,3,4,5;WKST=MO",
@@ -152,69 +157,69 @@ def test_parse_rrule_error(
                 "EXDATE;VALUE=DATE:19991231",
             ],
             True,
-            RecurrenceRule(
-                freq=Frequency.DAILY,
-                until=date(2000, 1, 1),
-                count=None,
-                interval=1,
-                bysecond=(0,),
-                byminute=(0,),
-                byhour=(0,),
-                byday=(
-                    (-2, Weekday.MO),
-                    (-1, Weekday.TU),
-                    (0, Weekday.WE),
-                    (1, Weekday.TH),
-                    (2, Weekday.FR),
+            Recurrence(
+                rrule=RecurrenceRule(
+                    freq=Frequency.DAILY,
+                    until=date(2000, 1, 1),
+                    count=None,
+                    interval=1,
+                    bysecond=(0,),
+                    byminute=(0,),
+                    byhour=(0,),
+                    byday=(
+                        (-2, Weekday.MO),
+                        (-1, Weekday.TU),
+                        (0, Weekday.WE),
+                        (1, Weekday.TH),
+                        (2, Weekday.FR),
+                    ),
+                    bymonthday=(1, 2, 3, 4, 5),
+                    byyearday=(1, 2, 3, 4, 5),
+                    byweekno=(1, 2, 3, 4, 5),
+                    bymonth=(1, 2, 3, 4, 5),
+                    bysetpos=(1, 2, 3, 4, 5),
+                    wkst=Weekday.MO,
                 ),
-                bymonthday=(1, 2, 3, 4, 5),
-                byyearday=(1, 2, 3, 4, 5),
-                byweekno=(1, 2, 3, 4, 5),
-                bymonth=(1, 2, 3, 4, 5),
-                bysetpos=(1, 2, 3, 4, 5),
-                wkst=Weekday.MO,
+                rdate=(date(2000, 1, 2), date(2000, 1, 3)),
+                exdate=(date(1999, 12, 31),),
             ),
-            (date(2000, 1, 2), date(2000, 1, 3)),
-            (date(1999, 12, 31),),
         ),
         (
             [
                 "RRULE:FREQ=HOURLY;UNTIL=20000101T123000Z;INTERVAL=1;BYSECOND=0,1,2;BYMONTHDAY=1,2,3;WKST=TU",
             ],
             False,
-            RecurrenceRule(
-                freq=Frequency.HOURLY,
-                until=datetime(2000, 1, 1, 12, 30, 0, tzinfo=UTC),
-                count=None,
-                interval=1,
-                bysecond=(0, 1, 2),
-                byminute=None,
-                byhour=None,
-                byday=None,
-                bymonthday=(1, 2, 3),
-                byyearday=None,
-                byweekno=None,
-                bymonth=None,
-                bysetpos=None,
-                wkst=Weekday.TU,
+            Recurrence(
+                rrule=RecurrenceRule(
+                    freq=Frequency.HOURLY,
+                    until=datetime(2000, 1, 1, 12, 30, 0, tzinfo=UTC),
+                    count=None,
+                    interval=1,
+                    bysecond=(0, 1, 2),
+                    byminute=None,
+                    byhour=None,
+                    byday=None,
+                    bymonthday=(1, 2, 3),
+                    byyearday=None,
+                    byweekno=None,
+                    bymonth=None,
+                    bysetpos=None,
+                    wkst=Weekday.TU,
+                ),
+                rdate=(),
+                exdate=(),
             ),
-            (),
-            (),
         ),
     ],
 )
 def test_parse_recurrence(
     recurrence_list: list[str],
     is_all_day: bool,
-    expected_rrule: RecurrenceRule,
-    expected_rdate: tuple[date, ...],
-    expected_exdate: tuple[date, ...],
+    expected_recurrence: Recurrence | None,
 ) -> None:
     recurrence = parse_recurrence(recurrence_list, is_all_day)
 
-    assert recurrence.rrule == expected_rrule
-    assert recurrence.rdate == expected_rdate
-    assert recurrence.exdate == expected_exdate
+    assert recurrence == expected_recurrence
 
 
 @pytest.mark.parametrize(
