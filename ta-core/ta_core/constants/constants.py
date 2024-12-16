e3bf514a0f214097562d44c323cdc9ff57392259
himura-shitara <mitarashidango0927@gmail.com>
2024-12-17 07:36:31 +0900

    feat: aurora connection test

--- ta-core/ta_core/constants/constants.py
+++ ta-core/ta_core/constants/constants.py
@@ -5,3 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 FRONTEND_URL = os.getenv("FRONTEND_URL")
+AWS_RDS_CLUSTER_INSTANCE_URL = os.getenv("AWS_RDS_CLUSTER_INSTANCE_URL")
+AWS_RDS_CLUSTER_MASTER_USERNAME = os.getenv("AWS_RDS_CLUSTER_MASTER_USERNAME")
