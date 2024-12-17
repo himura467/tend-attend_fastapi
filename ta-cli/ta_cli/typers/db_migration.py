a3355c253220e5c5dd1245e5fc57816126608a5d
himura-shitara <mitarashidango0927@gmail.com>
2024-12-17 09:18:23 +0900

    feat: create aurora db when migrate

--- ta-cli/ta_cli/typers/db_migration.py
+++ ta-cli/ta_cli/typers/db_migration.py
@@ -1,8 +1,9 @@
 import asyncio
 
 import typer
-from ta_core.infrastructure.sqlalchemy.migrate_db import reset_db_async, generate_ddl
 from ta_core.aws.aurora import execute
+from ta_core.constants.constants import AURORA_DATABASE_NAME
+from ta_core.infrastructure.sqlalchemy.migrate_db import generate_ddl, reset_db_async
 
 app = typer.Typer()
 
@@ -14,5 +15,6 @@ def migrate() -> None:
 
 @app.command("migrate-aurora")
 def migrate_aurora() -> None:
+    execute(f"CREATE DATABASE IF NOT EXISTS {AURORA_DATABASE_NAME}")
     for ddl_statement in generate_ddl():
         execute(ddl_statement)
--- ta-core/ta_core/aws/aurora.py
+++ ta-core/ta_core/aws/aurora.py
@@ -2,6 +2,7 @@ from aws_advanced_python_wrapper import AwsWrapperConnection
 from mysql.connector import Connect
 
 from ta_core.constants.constants import (
+    AURORA_DATABASE_NAME,
     AWS_RDS_CLUSTER_INSTANCE_URL,
     AWS_RDS_CLUSTER_MASTER_USERNAME,
 )
@@ -12,7 +13,7 @@ def execute(query: str) -> None:
     with AwsWrapperConnection.connect(
         Connect,
         host=AWS_RDS_CLUSTER_INSTANCE_URL,
-        database="mysql",
+        database=AURORA_DATABASE_NAME,
         user=AWS_RDS_CLUSTER_MASTER_USERNAME,
         password=AWS_RDS_CLUSTER_MASTER_PASSWORD,
         plugins="failover",
