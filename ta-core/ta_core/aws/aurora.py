e1fb902fa61962e7f061fd6511c42c1986f9d635
himura467 <113117363+himura467@users.noreply.github.com>
2024-12-18 19:27:25 +0900

    Amazon Aurora との接続において、dbname を指定できるようにする (#74)

--- ta-core/ta_core/aws/aurora.py
+++ ta-core/ta_core/aws/aurora.py
@@ -2,18 +2,17 @@ from aws_advanced_python_wrapper import AwsWrapperConnection
 from mysql.connector import Connect
 
 from ta_core.constants.constants import (
-    AURORA_DATABASE_NAME,
     AWS_RDS_CLUSTER_INSTANCE_URL,
     AWS_RDS_CLUSTER_MASTER_USERNAME,
 )
 from ta_core.constants.secrets import AWS_RDS_CLUSTER_MASTER_PASSWORD
 
 
-def execute(query: str) -> None:
+def execute(query: str, dbname: str) -> None:
     with AwsWrapperConnection.connect(
         Connect,
         host=AWS_RDS_CLUSTER_INSTANCE_URL,
-        database=AURORA_DATABASE_NAME,
+        database=dbname,
         user=AWS_RDS_CLUSTER_MASTER_USERNAME,
         password=AWS_RDS_CLUSTER_MASTER_PASSWORD,
         plugins="failover",
