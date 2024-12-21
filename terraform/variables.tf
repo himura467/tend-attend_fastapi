630e4d1e664e6f9425b182e2d9734c954f1f5ac6
himura467 <113117363+himura467@users.noreply.github.com>
2024-12-22 06:13:58 +0900

    Terraform のリソース定義をいい感じに再設計 (#92)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,16 @@ variable "lambda_function_name" {
   type = string
   default = "tend-attend-lambda-function"
 }
+
+variable "frontend_url" {
+  description = "URL of the frontend"
+  type = string
+  default = "https://tend-attend.vercel.app"
+}
+
+variable "db_shard_count" {
+  description = "Number of shards for the DB"
+  type = number
+  // 現状のデプロイの仕方だと、1以外の値を指定するとエラーになる
+  default = 1
+}
