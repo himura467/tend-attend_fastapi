b99997c7c929de06b7d870f6c5d3cec5a20493a8
himura467 <113117363+himura467@users.noreply.github.com>
2024-12-23 06:25:26 +0900

    （多分）AWS Secrets Manager のキーの問題で DB に接続できていないので、#93 を revert (#95)

--- ta-core/ta_core/aws/aurora.py
+++ ta-core/ta_core/aws/aurora.py
@@ -1,11 +1,11 @@
 from aws_advanced_python_wrapper import AwsWrapperConnection
 from mysql.connector import Connect
 
-from ta_core.constants.constants import AWS_RDS_CLUSTER_INSTANCE_URL
-from ta_core.constants.secrets import (
-    AWS_SECRETSMANAGER_REGION,
-    AWS_SECRETSMANAGER_SECRET_ID,
+from ta_core.constants.constants import (
+    AWS_RDS_CLUSTER_INSTANCE_URL,
+    AWS_RDS_CLUSTER_MASTER_USERNAME,
 )
+from ta_core.constants.secrets import AWS_RDS_CLUSTER_MASTER_PASSWORD
 
 
 def execute(query: str, dbname: str) -> None:
@@ -13,9 +13,9 @@ def execute(query: str, dbname: str) -> None:
         Connect,
         host=AWS_RDS_CLUSTER_INSTANCE_URL,
         database=dbname,
-        secrets_manager_secret_id=AWS_SECRETSMANAGER_SECRET_ID,
-        secrets_manager_region=AWS_SECRETSMANAGER_REGION,
-        plugins="aws_secrets_manager",
+        user=AWS_RDS_CLUSTER_MASTER_USERNAME,
+        password=AWS_RDS_CLUSTER_MASTER_PASSWORD,
+        plugins="failover",
         wrapper_dialect="aurora-mysql",
         autocommit=True,
     ) as awsconn, awsconn.cursor() as cursor:
