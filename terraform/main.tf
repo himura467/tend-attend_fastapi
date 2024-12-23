aad7d16e50fa7b7411daeba2e5a21bda2b3ba3fc
himura467 <mitarashidango0927@gmail.com>
2024-12-24 08:47:02 +0900

    feat: not to use mocked response

--- terraform/main.tf
+++ terraform/main.tf
@@ -292,11 +292,6 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = aws_lambda_function.tend_attend_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "lambda_deployment" {
-  depends_on  = [ aws_api_gateway_integration.lambda_integration ]
-  rest_api_id = aws_api_gateway_rest_api.tend_attend_api.id
-}
-
 resource "aws_api_gateway_method" "proxy_options" {
   rest_api_id   = aws_api_gateway_rest_api.tend_attend_api.id
   resource_id   = aws_api_gateway_resource.proxy.id
@@ -305,39 +300,17 @@ resource "aws_api_gateway_method" "proxy_options" {
 }
 
 resource "aws_api_gateway_integration" "proxy_options" {
-  rest_api_id       = aws_api_gateway_rest_api.tend_attend_api.id
-  resource_id       = aws_api_gateway_resource.proxy.id
-  http_method       = aws_api_gateway_method.proxy_options.http_method
-  type              = "MOCK"
-  request_templates = {
-    "application/json" = "{\"statusCode\": 200}"
-  }
+  rest_api_id             = aws_api_gateway_rest_api.tend_attend_api.id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy_options.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.tend_attend_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "proxy_options" {
-  rest_api_id         = aws_api_gateway_rest_api.tend_attend_api.id
-  resource_id         = aws_api_gateway_resource.proxy.id
-  http_method         = aws_api_gateway_method.proxy_options.http_method
-  status_code         = 200
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers"     = true
-    "method.response.header.Access-Control-Allow-Methods"     = true
-    "method.response.header.Access-Control-Allow-Origin"      = true
-    "method.response.header.Access-Control-Allow-Credentials" = true
-  }
-}
-
-resource "aws_api_gateway_integration_response" "proxy_options" {
-  rest_api_id         = aws_api_gateway_rest_api.tend_attend_api.id
-  resource_id         = aws_api_gateway_resource.proxy.id
-  http_method         = aws_api_gateway_method.proxy_options.http_method
-  status_code         = aws_api_gateway_method_response.proxy_options.status_code
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers"     = "'Content-Type,X-Api-Key,Authorization'"
-    "method.response.header.Access-Control-Allow-Methods"     = "'GET,POST,PUT,DELETE,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"      = "'${var.frontend_url}'"
-    "method.response.header.Access-Control-Allow-Credentials" = "'true'"
-  }
+resource "aws_api_gateway_deployment" "lambda_deployment" {
+  depends_on  = [ aws_api_gateway_integration.lambda_integration, aws_api_gateway_integration.proxy_options ]
+  rest_api_id = aws_api_gateway_rest_api.tend_attend_api.id
 }
 
 resource "aws_cloudwatch_log_group" "api_gw" {
