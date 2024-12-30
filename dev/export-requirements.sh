9d02549da37436292960413692a8355551293670
himura467 <113117363+himura467@users.noreply.github.com>
2024-12-30 10:27:58 +0900

    ta-cli に属する依存関係を requirements.txt に含めないようにする (#108)

--- dev/export-requirements.sh
+++ dev/export-requirements.sh
@@ -5,6 +5,17 @@ set -e
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ROOT_DIR=${SCRIPT_DIR}/..
 
-${SCRIPT_DIR}/poetry-all.sh export -f requirements.txt -o requirements.txt --without-hashes
+projects=(
+  "ta-api"
+  "ta-core"
+  "ta-ml"
+)
+
+for project in "${projects[@]}"; do
+  echo "Running export for ${project}"
+  cd ${ROOT_DIR}/${project}
+  poetry export -f requirements.txt -o requirements.txt --without-hashes
+done
+
 grep -hv "tend-attend" ${ROOT_DIR}/*/requirements.txt | sort -u -o ${ROOT_DIR}/requirements.txt
 rm ${ROOT_DIR}/*/requirements.txt
