3bd3863894259040cbc89c1c251af72a17b414dc
himura467 <mitarashidango0927@gmail.com>
2025-02-02 14:53:41 +0900

    feat: api constants

---
+++ ta-api/ta_api/constants.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+_FRONTEND_URLS = os.getenv("FRONTEND_URLS")
+assert _FRONTEND_URLS, "FRONTEND_URLS environment variable must be set"
+ALLOWED_ORIGINS = _FRONTEND_URLS.split(",")
+
+ACCESS_TOKEN_NAME = "acctkn"
+REFRESH_TOKEN_NAME = "reftkn"
