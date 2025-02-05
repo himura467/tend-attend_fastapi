33376d0354d942a65165f1a12fc46636ac863b2c
himura467 <mitarashidango0927@gmail.com>
2025-02-06 00:32:16 +0900

    feat: uuid_to_bin, bin_to_uuid

--- ta-core/ta_core/utils/uuid.py
+++ ta-core/ta_core/utils/uuid.py
@@ -1,5 +1,15 @@
+import uuid
+
 import uuid6
 
 
-def generate_uuid() -> str:
-    return str(uuid6.uuid7())
+def generate_uuid() -> uuid.UUID:
+    return uuid6.uuid7()
+
+
+def uuid_to_bin(u: uuid.UUID) -> bytes:
+    return u.bytes
+
+
+def bin_to_uuid(b: bytes) -> uuid.UUID:
+    return uuid.UUID(bytes=b)
