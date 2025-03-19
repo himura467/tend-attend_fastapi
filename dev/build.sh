a88aef2879e7278f71b35839e0264af5b3191d0b
himura467 <mitarashidango0927@gmail.com>
2025-03-20 00:27:15 +0900

    refactor: export requirements in a subshell

--- dev/build.sh
+++ dev/build.sh
@@ -4,7 +4,6 @@ set -e
 
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 
-source ${ROOT_DIR}/dev/export-requirements.sh
-cd ${ROOT_DIR}
+bash ${ROOT_DIR}/dev/export-requirements.sh
 docker build -f ${ROOT_DIR}/docker/lambda-server/Dockerfile -t tend-attend --no-cache --provenance=false . --progress=plain
 rm ${ROOT_DIR}/requirements.txt
