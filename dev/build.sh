c51c30270963e57b63600dd81990361f1accb8f5
himura467 <mitarashidango0927@gmail.com>
2025-04-01 22:12:15 +0900

    WIP: lambda_handler を呼び出す方法が分からない

--- dev/build.sh
+++ dev/build.sh
@@ -4,6 +4,10 @@ set -e
 
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 
+PYTHON_VERSION=$(cat ${ROOT_DIR}/.python-version)
+
 bash ${ROOT_DIR}/dev/export-requirements.sh
-docker build -f ${ROOT_DIR}/docker/lambda-server/Dockerfile -t tend-attend --no-cache --provenance=false . --progress=plain
+docker build -f ${ROOT_DIR}/docker/lambda-server/Dockerfile \
+  --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
+  -t tend-attend --no-cache --provenance=false . --progress=plain
 rm ${ROOT_DIR}/requirements.txt
