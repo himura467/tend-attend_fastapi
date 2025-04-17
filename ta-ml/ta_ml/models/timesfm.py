7d69a12e6d391e1b207286a1c6fa86f208f953b3
himura467 <mitarashidango0927@gmail.com>
2025-04-17 16:33:53 +0900

    feat: enable to load local checkpoint

--- ta-ml/ta_ml/models/timesfm.py
+++ ta-ml/ta_ml/models/timesfm.py
@@ -1,4 +1,5 @@
 import timesfm
+from ta_core.constants.constants import CHECKPOINT_PATH
 
 from ta_ml.constants import models
 
@@ -10,6 +11,7 @@ tfm = timesfm.TimesFm(
         num_layers=50,  # Usage に書いてあった値をそのまま使っているだけで特に意味はない
     ),
     checkpoint=timesfm.TimesFmCheckpoint(
+        path=CHECKPOINT_PATH,
         huggingface_repo_id=models.CHECKPOINT_REPO_ID,
     ),
 )
